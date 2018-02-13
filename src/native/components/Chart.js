db76e0a6c9d8d0141e4a6d328420f806da994ade
Artem Kashin <dev.tema@gmail.com>
2018-02-13 14:54:20 +0300

    charts init

--- src/native/components/Chart.js
+++ src/native/components/Chart.js
@@ -2,8 +2,55 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { View } from 'native-base';
 
+import { AreaChart, YAxis } from 'react-native-svg-charts'
+import { LinearGradient, Stop, G, Line } from 'react-native-svg'
+import * as shape from 'd3-shape'
+
+const data = [ 500, 100, 400, 950, 50, 850, 910, 350, 530, 40, 240, 500, 900, 1500, 400, 950, 50, 850, 910, 350, 530, 40, 240, 500, 930, 1400, 2320, 4100, 6730, 3750, 4500 ];
+const contentInset = { top: 20, bottom: 20 };
+
+const CustomGrid = ({ x, y, dataPoints, ticks }) => (
+  <G>
+    {
+      [ 50, 3200, 6730 ].map(tick => (
+        <Line
+          key={tick}
+          x1={'0%'}
+          x2={'100%'}
+          y1={y(tick)}
+          y2={y(tick)}
+          stroke={'#2F2A40'}
+        />
+      ))
+    }
+  </G>
+)
+
 const Chart = ({ size }) => (
-  <View style={{ flex: 1, height: size, borderColor: '#2F2A40', borderTopWidth: 1, marginBottom: 10 }} />
+  <View style={{ height: 170, flexDirection: 'row', flexWrap: 'wrap', marginBottom: 10, position: 'relative' }}>
+    <YAxis
+      style={{ position: 'absolute', top: 0, bottom: 0, left: 5 }}
+      dataPoints={data}
+      numberOfTicks={3}
+      contentInset={contentInset}
+      labelStyle={{ color: '#8D8A96' }}
+      formatLabel={value => `${value.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}
+    />
+    <AreaChart
+      style={{ flex: 1 }}
+      dataPoints={data}
+      svg={{ stroke: '#31E981' }}
+      contentInset={contentInset}
+      curve={shape.curveLinear}
+      renderGradient={({ id }) => (
+        <LinearGradient id={id} x1={'0%'} y1={'0%'} x2={'0%'} y2={'100%'}>
+          <Stop offset={'0%'} stopColor={'#31E981'} stopOpacity={0.2}/>
+          <Stop offset={'100%'} stopColor={'#31E981'} stopOpacity={0}/>
+        </LinearGradient>
+      )}
+      renderGrid={CustomGrid}
+    />
+  </View>
 );
 
 Chart.propTypes = {
--- src/native/components/Coin.js
+++ src/native/components/Coin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StatusBar } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import { Container, Content, Card, CardItem, Body, H3, List, ListItem, Text, Header, Left, Button, Title, Right } from 'native-base';
 import ErrorMessages from '../../constants/errors';
 import Error from './Error';
@@ -8,6 +8,10 @@ import Spacer from './Spacer';
 import Icon from './Icon';
 import CoinCard from './CoinCard';
 import { Actions } from 'react-native-router-flux';
+
+import { AreaChart, YAxis } from 'react-native-svg-charts'
+import { LinearGradient, Stop } from 'react-native-svg'
+import * as shape from 'd3-shape'
 
 const CoinView = ({
   error,
@@ -30,6 +34,9 @@ const CoinView = ({
   // Coin not found
   if (!coin) return <Error content={ErrorMessages.coin404} />;
 
+  const data = [ 50, 10, 40, 95, 5, 85, 91, 35, 53, 4, 24, 50 ];
+  const contentInset = { top: 20, bottom: 20 };
+
   return (
     <Container>
       <Header style={{ backgroundColor: '#1B152D', borderBottomWidth: 0 }}>
@@ -45,6 +52,29 @@ const CoinView = ({
         <Right></Right>
       </Header>
       <Content padder style={{ backgroundColor: '#1B152D' }}>
+        <View style={{ height: 200, flexDirection: 'row' }}>
+          <YAxis
+              dataPoints={data}
+              contentInset={contentInset}
+              labelStyle={{ color: '#8D8A96' }}
+              formatLabel={value => `${value}`}
+          />
+          <AreaChart
+            style={ { flex: 1, marginLeft: 20 } }
+            dataPoints={ data }
+            svg={{
+              stroke: '#31E981',
+            }}
+            contentInset={contentInset}
+            curve={shape.curveLinear}
+            renderGradient={({ id }) => (
+              <LinearGradient id={id} x1={'0%'} y1={'0%'} x2={'0%'} y2={'100%'}>
+                <Stop offset={'0%'} stopColor={'#31E981'} stopOpacity={0.2}/>
+                <Stop offset={'100%'} stopColor={'#31E981'} stopOpacity={0}/>
+              </LinearGradient>
+            )}
+          />
+        </View>
         <Spacer size={25} />
         <CoinCard
           key={coin.id}
