6d1d5cf6bf01b320fb631f4559439185ea8a3a1e
Artem Kashin <dev.tema@gmail.com>
2018-05-17 08:46:10 +0300

    interactable lib

--- android/app/src/main/java/com/coinsane/v1/MainApplication.java
+++ android/app/src/main/java/com/coinsane/v1/MainApplication.java
@@ -3,6 +3,7 @@ package com.coinsane.v1;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.wix.interactable.Interactable;
 import com.reactNativeQuickActions.AppShortcutsPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.apsl.versionnumber.RNVersionNumberPackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new Interactable(),
             new AppShortcutsPackage(),
             new RNI18nPackage(),
             new RNVersionNumberPackage(),
